f3f4150b96ff33a9b2291679533c63929f67f297
Naomi Isabel Sanchez Chavarria <naomi.sanchez@tecsup.edu.pe>
2026-05-13 17:45:21 -0500

    feat: implement shopping cart

---
+++ src/context/CartContext.tsx
@@ -0,0 +1,33 @@
+import { createContext, useContext, useState } from 'react'
+import type { Product } from '../types/Product'
+
+type CartContextType = {
+  cart: Product[]
+  addToCart: (product: Product) => void
+}
+
+const CartContext = createContext<CartContextType | null>(null)
+
+export function CartProvider({ children }: { children: React.ReactNode }) {
+  const [cart, setCart] = useState<Product[]>([])
+
+  const addToCart = (product: Product) => {
+    setCart((prev) => [...prev, product])
+  }
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart }}>
+      {children}
+    </CartContext.Provider>
+  )
+}
+
+export function useCart() {
+  const context = useContext(CartContext)
+
+  if (!context) {
+    throw new Error('useCart must be used inside CartProvider')
+  }
+
+  return context
+}
--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,5 +1,19 @@
+import { useCart } from '../context/CartContext'
+
 function CartPage() {
-  return <h2>Carrito</h2>
+  const { cart } = useCart()
+
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">Carrito</h2>
+
+      {cart.map((item, index) => (
+        <p key={index}>
+          {item.title} - S/ {item.price}
+        </p>
+      ))}
+    </div>
+  )
 }
 
 export default CartPage
