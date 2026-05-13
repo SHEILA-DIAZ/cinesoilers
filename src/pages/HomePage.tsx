b3d0ad09e641c04d8c10045bb7e2b16cda038456
Naomi Isabel Sanchez Chavarria <naomi.sanchez@tecsup.edu.pe>
2026-05-13 17:20:21 -0500

    feat: install router axios and tailwind

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,9 @@
 function HomePage() {
-  return <h2>Bienvenida a CineSpoilerS</h2>
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">CineSpoilerS</h1>
+    </div>
+  )
 }
 
 export default HomePage
